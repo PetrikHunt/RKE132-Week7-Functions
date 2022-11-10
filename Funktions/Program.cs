7e32dcfd1203e42d46559ac513b4c1a3b9074d94
PetrikHunt <petrikhuntofficial@gmail.com>
2022-11-10 14:11:25 +0200

    create new app

---
+++ Funktions/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Are ypu coming or not? (in/out)");
+string userChoise = Console.ReadLine();
+
+if(userChoise == "in")
+{
+    PrintHello();
+}
+else
+{
+    PrintGoodBye(); 
+}
+
+
+
+
+static void PrintHello() //funktion ehk meetod
+{
+    Console.WriteLine("Hello, world!");
+}
+
+static void PrintGoodBye() //funktion ehk meetod
+{
+    Console.WriteLine("See you later!");
+}
